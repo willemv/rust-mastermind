dbe1405b29f2e0e417f003223a01305143345ac6
Willem Verstraeten <willem.verstraeten@gmail.com>
2020-05-02 22:25:48 +0200

    starting to put functionality in the web page

--- wasm/www/index.js
+++ wasm/www/index.js
@@ -1,11 +1,35 @@
 import * as wasm from "mastermind-wasm";
+import * as shuffle from "shuffle-array";
 
 wasm.init();
 
-document.getElementById("button_check").onclick = function(event) {
-    wasm.greet();
+let all_colors = ["R", "G", "Y", "B", "C", "P"];
+
+function generate_secret() {
+    return shuffle.pick(all_colors, { 'picks': 4 }).join("");
 }
 
-document.getElementById("button_panic").onclick = function(event) {
-    wasm.trigger_panic();
+let secret = generate_secret();
+
+let results = document.getElementById("results");
+
+document.getElementById("button_generate_secret").onclick = function(event) {
+    secret = generate_secret();
+    console.log(secret);
+    while(results.hasChildNodes()) {
+        results.removeChild(results.firstChild);
+    }
+}
+
+document.getElementById("check_form").onsubmit = function(event) {
+    let guess = this.guess.value;
+    this.guess.value = "";
+    let grade = wasm.grade(guess, secret)
+
+    let new_result = document.createElement("div");
+    new_result.innerText = guess + " -> " + grade;
+    results.appendChild(new_result);
+
+    event.preventDefault();
+    return true;
 }
