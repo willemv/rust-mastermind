f8cce439e2513a9a1456360b1e1bf4fc35bbc90d
Willem Verstraeten <willem.verstraeten@gmail.com>
2020-05-02 22:27:47 +0200

    fix static analysis warnings

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,7 +2,6 @@ extern crate mastermind;
 
 mod utils;
 
-use mastermind::Color::*;
 use mastermind::*;
 use wasm_bindgen::prelude::*;
 
@@ -29,52 +28,48 @@ pub fn trigger_panic() {
 
 #[wasm_bindgen]
 pub fn grade(guess: String, solution: String) -> String {
-    let guess  = parse_colors(&guess);
+    let guess = parse_colors(&guess);
     let solution = parse_colors(&solution);
 
     match (guess, solution) {
-        (Ok(guess), Ok(solution)) => {
-            match mastermind::grade(&guess, &solution) {
-                Grade::Correct => "CORRECT".to_string(),
-                Grade::Incorrect{
-                    correct_position,
-                    correct_color,
-                    wrong
-                } => {
-                    let mut result = String::with_capacity(guess.len());
-                    for _ in 0..correct_position {
-                        result.push('X');
-                    }
-                    for _ in 0..correct_color {
-                        result.push('O');
-                    }
-                    for _ in 0..wrong {
-                        result.push('.');
-                    }
-                    result
+        (Ok(guess), Ok(solution)) => match mastermind::grade(&guess, &solution) {
+            Grade::Correct => "CORRECT".to_string(),
+            Grade::Incorrect {
+                correct_position,
+                correct_color,
+                wrong,
+            } => {
+                let mut result = String::with_capacity(guess.len());
+                for _ in 0..correct_position {
+                    result.push('X');
                 }
-                Grade::Invalid(_) => "INVALID".to_string(),
+                for _ in 0..correct_color {
+                    result.push('O');
+                }
+                for _ in 0..wrong {
+                    result.push('.');
+                }
+                result
             }
-        }
-        (Ok(_), Err(message)) => "PARSE_ERROR GUESS".to_string(),
-        (Err(message), Ok(_)) => "PARSE_ERROR SOLUTION".to_string(),
-        (Err(m1), Err(m2)) => "PARSE_ERROR_GUESS_AND_SOLUTION".to_string(),
+            Grade::Invalid(_) => "INVALID".to_string(),
+        },
+        (Ok(_), Err(_)) => "PARSE_ERROR GUESS".to_string(),
+        (Err(_), Ok(_)) => "PARSE_ERROR SOLUTION".to_string(),
+        (Err(_), Err(_)) => "PARSE_ERROR_GUESS_AND_SOLUTION".to_string(),
     }
-   
-
 }
 
 fn parse_colors(string: &str) -> Result<Vec<Color>, &str> {
     let mut result = Vec::with_capacity(string.len());
     for char in string.chars() {
-         match char {
+        match char {
             'R' => result.push(Color::RED),
             'Y' => result.push(Color::YELLOW),
             'G' => result.push(Color::GREEN),
             'B' => result.push(Color::BLUE),
             'C' => result.push(Color::CYAN),
             'P' => result.push(Color::PURPLE),
-            _ => return Err("invalid color")
+            _ => return Err("invalid color"),
         };
     }
     Ok(result)
