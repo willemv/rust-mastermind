0fc0ad00b3df7c64ca25b7838cae72946b9959cc
Willem Verstraeten <willem.verstraeten@gmail.com>
2020-06-06 13:05:16 +0200

    use pegs to input guess, not a textfield

--- wasm/www/index.js
+++ wasm/www/index.js
@@ -1,10 +1,11 @@
 import * as wasm from "mastermind-wasm";
-import * as shuffle from "shuffle-array";
+import shuffle from "shuffle-array";
 import * as templ from "./microtemplating";
 
 wasm.init();
 
 let all_colors = ["R", "G", "Y", "B", "C", "P"];
+let all_color_names = ["red", "green", "yellow", "blue", "cyan", "purple"];
 
 function generate_secret() {
     return shuffle.pick(all_colors, { 'picks': 4 }).join("");
@@ -14,16 +15,15 @@ let secret = generate_secret();
 
 let results = document.getElementById("results");
 
-document.getElementById("button_generate_secret").onclick = function (event) {
+document.getElementById("button_generate_secret").addEventListener("click", event => {
     secret = generate_secret();
     console.log(secret);
     while (results.hasChildNodes()) {
         results.removeChild(results.firstChild);
     }
-}
+});
 
-function symbol_to_color(symbol) {
-    
+function color_symbol_to_color_name(symbol) {
     if (symbol == "R") {
         return "red";
     }
@@ -42,7 +42,29 @@ function symbol_to_color(symbol) {
     if (symbol == "P") {
         return "purple";
     }
-    throw new Error("Unknown symbol: " + symbol);
+    throw new Error("Unknown color symbol: " + symbol);
+}
+
+function color_name_to_color_symblol(color_name) {
+    if (color_name == "red") {
+        return  "R";
+    }
+    if (color_name == "green") {
+        return  "G";
+    }
+    if (color_name == "yellow") {
+        return  "Y";
+    }
+    if (color_name == "blue") {
+        return  "B";
+    }
+    if (color_name == "cyan") {
+        return  "C";
+    }
+    if (color_name == "purple") {
+        return  "P";
+    }
+    throw new Error("Unknown color name: " + color_name);
 }
 
 function grade_to_class(grade) {
@@ -58,8 +80,7 @@ function grade_to_class(grade) {
     throw new Error("Uknown grade: " + grade);
 }
 
-function check(guess) {
-    
+function process_guess(guess) {
     let grade = wasm.grade(guess, secret);
     if (!grade.match(/^[XO.]+$/)) {
         console.error(grade);
@@ -67,23 +88,59 @@ function check(guess) {
     }
 
     let template_data = {
-        guess: guess.split('').map(symbol => {return {color: symbol_to_color(symbol), symbol: symbol}}),
+        guess: guess.split('').map(symbol => {return {color: color_symbol_to_color_name(symbol), symbol: symbol}}),
         result: grade.split('').map(grade => {return {symbol: grade, grade: grade_to_class(grade)}}),
     };
     results.innerHTML = results.innerHTML + templ.tmpl("template_grade", template_data);
     return true;
 }
 
-document.getElementById("check_form").onsubmit = function (event) {
-    let guess = this.guess.value;
-    try{
-        let check_valid = check(guess);
-        if (check_valid) {
-            this.guess.value = "";
-        }
-    } catch (error) {
-        console.log(error);
+function next_color_after(color) {
+    let current_index = all_color_names.findIndex(c => c === color);
+    if (current_index >= 0) {
+        return all_color_names[ (current_index + 1) % all_color_names.length];
+    } else {
+        return undefined;
     }
-    event.preventDefault();
-    return true;
 }
+
+let guess_form = document.getElementById("guess_form");
+let guess_pegs = guess_form.querySelectorAll(".peg");
+guess_pegs.forEach(peg => {
+    peg.addEventListener("click", function(event) {
+        event.preventDefault();
+        //todo show color wheel
+        let next_color = "red"; //provide a default
+        let current_color = this.dataset.color;
+        if (current_color) {
+            next_color = next_color_after(current_color);
+        }
+        this.dataset.color = next_color;
+        return false;
+    });
+});
+
+function convert_names_to_guess(color_names) {
+    for (let name in color_names) {
+        console.log(color_names[name]);
+        if (!color_names[name]) return undefined;
+    }
+    return color_names.map(name => color_name_to_color_symblol(name)).join("");
+}
+
+guess_form.addEventListener("submit", event => {
+    event.preventDefault();
+    let guess_color_names = Array.from(guess_pegs).map(peg => peg.dataset.color);
+    let guess = convert_names_to_guess(guess_color_names);
+
+    if (!guess) {
+        console.error("Guess a color for each peg");
+    } else {
+        try {
+            process_guess(guess);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return true;
+});
